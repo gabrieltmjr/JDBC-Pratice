109f50e8cc5ff2a3337343251ae6e8e862f2edba
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-14 12:01:00 +0000

    wrote some tasks that this class needs to do + empty constructor + empty connectToDatabase() method

--- src/main/java/com/gabrieltmjr/jdbcProject/controller/PersonController.java
+++ src/main/java/com/gabrieltmjr/jdbcProject/controller/PersonController.java
@@ -1,4 +1,17 @@
 package com.gabrieltmjr.jdbcProject.controller;
+import java.sql.*;
 
 public class PersonController {
+
+    //Connect to Database
+    public PersonController() {
+
+    }
+    private void connectToDatabase() {
+
+    }
+    //Read from database
+    //Insert into database
+    //Update to database
+    //Delete from database
 }
