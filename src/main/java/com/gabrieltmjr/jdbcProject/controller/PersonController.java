9188b455fe9246409c9d87a46156b446186ec927
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-17 12:18:05 +0000

    Insert and read from database works

--- src/main/java/com/gabrieltmjr/jdbcProject/controller/PersonController.java
+++ src/main/java/com/gabrieltmjr/jdbcProject/controller/PersonController.java
@@ -1,17 +1,53 @@
 package com.gabrieltmjr.jdbcProject.controller;
+import com.gabrieltmjr.jdbcProject.model.Person;
+
 import java.sql.*;
+import java.util.Vector;
 
 public class PersonController {
 
-    //Connect to Database
-    public PersonController() {
+    Connection connection;
+    Vector<Person> people;
+
+    public PersonController(Vector<Person> people) {
+        try {
+            connectToDatabase();
+            this.people = people;
+        } catch (SQLException s) {
+            System.out.println(s.getMessage());
+        }
+
 
     }
-    private void connectToDatabase() {
-
+    //Connect to Database
+    private void connectToDatabase() throws SQLException {
+        String url = "jdbc:sqlite:src/main/resources/database/example";
+        connection = DriverManager.getConnection(url);
     }
     //Read from database
+    public void checkAllData() throws SQLException {
+        Statement selectAll = connection.createStatement();
+        ResultSet all = selectAll.executeQuery("SELECT * FROM Person");
+        while (all.next()) {
+            int numColumns = all.getMetaData().getColumnCount();
+            for(int i = 1; i <= numColumns; i++) {
+                System.out.println("COLUMN"+i+"="+all.getObject(i));
+            }
+        }
+    }
     //Insert into database
+    public void insertPerson(Person person, Vector<Person> people) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO Person VALUES(?,?,?)");
+        statement.setInt(1,person.getIdNumber());
+        statement.setString(2,person.getName());
+        statement.setByte(3,person.getAge());
+        statement.executeUpdate();
+    }
     //Update to database
     //Delete from database
+
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
