42ca5c1e85162378d980c5581e414896c5a1cf00
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-20 10:58:29 +0000

    data to populate database

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,120 @@
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (1,"Audra",11),
+  (2,"Ora",54),
+  (3,"Rudyard",1),
+  (4,"Camille",98),
+  (5,"Kareem",82),
+  (6,"Boris",94),
+  (7,"Aladdin",56),
+  (8,"Shelby",33),
+  (9,"Wendy",97),
+  (10,"Boris",8);
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (11,"Colorado",70),
+  (12,"Ralph",6),
+  (13,"Farrah",21),
+  (14,"Zephania",17),
+  (15,"Jin",7),
+  (16,"Carlos",5),
+  (17,"Connor",25),
+  (18,"Brenden",97),
+  (19,"Quynn",12),
+  (20,"Shoshana",76);
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (21,"Adrian",38),
+  (22,"Drew",77),
+  (23,"Armando",79),
+  (24,"Jordan",41),
+  (25,"Nevada",32),
+  (26,"Oren",3),
+  (27,"Lawrence",20),
+  (28,"Abraham",53),
+  (29,"Anastasia",78),
+  (30,"Price",21);
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (31,"Bell",37),
+  (32,"Colt",32),
+  (33,"Curran",92),
+  (34,"Petra",54),
+  (35,"Rama",42),
+  (36,"Shad",76),
+  (37,"Reagan",25),
+  (38,"Dane",80),
+  (39,"Benedict",52),
+  (40,"Hyatt",44);
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (41,"Frances",20),
+  (42,"Gavin",4),
+  (43,"Benjamin",57),
+  (44,"Maisie",33),
+  (45,"Guy",40),
+  (46,"Maxine",60),
+  (47,"Nathaniel",21),
+  (48,"Cruz",2),
+  (49,"Isabella",40),
+  (50,"Dorian",21);
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (51,"Raymond",10),
+  (52,"Kirsten",77),
+  (53,"Cherokee",11),
+  (54,"Daniel",7),
+  (55,"Russell",65),
+  (56,"Arthur",73),
+  (57,"Michelle",48),
+  (58,"Madeline",35),
+  (59,"Aileen",31),
+  (60,"Warren",10);
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (61,"Selma",84),
+  (62,"Colton",90),
+  (63,"Benjamin",94),
+  (64,"Caleb",50),
+  (65,"Malik",46),
+  (66,"Grace",20),
+  (67,"Barbara",17),
+  (68,"Kevyn",96),
+  (69,"Lester",50),
+  (70,"Iliana",20);
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (71,"Halee",78),
+  (72,"Unity",95),
+  (73,"August",18),
+  (74,"Kenyon",22),
+  (75,"Fletcher",19),
+  (76,"Eaton",58),
+  (77,"Cameran",88),
+  (78,"Susan",74),
+  (79,"Ignacia",93),
+  (80,"Fulton",23);
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (81,"Bevis",37),
+  (82,"Ryder",63),
+  (83,"Charlotte",30),
+  (84,"Kristen",76),
+  (85,"Giacomo",12),
+  (86,"Haviva",64),
+  (87,"Dylan",90),
+  (88,"Dustin",55),
+  (89,"Amal",4),
+  (90,"Pascale",69);
+INSERT INTO Person (idNumber,name,age)
+VALUES
+  (91,"Magee",81),
+  (92,"Cooper",20),
+  (93,"Carter",50),
+  (94,"Louis",82),
+  (95,"Oscar",29),
+  (96,"Kirk",47),
+  (97,"Kylynn",89),
+  (98,"Silas",2),
+  (99,"Ivan",50),
+  (100,"Stone",35);
