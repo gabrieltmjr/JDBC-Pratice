bbb723ad9182d5bb1b6bbb0b880813e24fc85756
gabrieltmjr <gabrieltmjr@gmail.com>
2022-01-20 10:58:49 +0000

    PersonViewer queries working

--- src/main/java/com/gabrieltmjr/jdbcProject/Application.java
+++ src/main/java/com/gabrieltmjr/jdbcProject/Application.java
@@ -1,7 +1,16 @@
 package com.gabrieltmjr.jdbcProject;
 
+import com.gabrieltmjr.jdbcProject.viewer.PersonViewer;
+
+import java.sql.SQLException;
+
 public class Application {
     public static void main(String[] args) {
-
+        PersonViewer personViewer = new PersonViewer();
+        try {
+            personViewer.viewByEqualName("Peter");
+        } catch (SQLException s) {
+            System.out.println(s.getMessage());
+        }
     }
 }
--- src/main/java/com/gabrieltmjr/jdbcProject/viewer/PersonViewer.java
+++ src/main/java/com/gabrieltmjr/jdbcProject/viewer/PersonViewer.java
@@ -1,4 +1,102 @@
 package com.gabrieltmjr.jdbcProject.viewer;
 
+import java.sql.*;
+
 public class PersonViewer {
+
+    Connection connection;
+    PreparedStatement statement;
+    ResultSet resultSet;
+    public PersonViewer() {
+        try {
+            connectToDatabase();
+        } catch (SQLException s) {
+            System.out.println(s.getMessage());
+        }
+    }
+
+    //Connect to Database
+    private void connectToDatabase() throws SQLException {
+        String url = "jdbc:sqlite:src/main/resources/database/example";
+        connection = DriverManager.getConnection(url);
+    }
+
+    public void viewAllPeople() throws SQLException {
+        statement = connection.prepareStatement("SELECT * FROM Person");
+        resultSet = statement.executeQuery();
+        System.out.println("idNumber |   name   |  age  |");
+        System.out.println("=============================");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getObject(1)+"    "+resultSet.getObject(2)+"    "+resultSet.getObject(3));
+        }
+    }
+
+    public void viewPerson(int idNumber) throws SQLException {
+        statement = connection.prepareStatement("SELECT * FROM Person WHERE idNumber = ?");
+        statement.setInt(1,idNumber);
+        resultSet = statement.executeQuery();
+        System.out.println("idNumber |   name   |  age  |");
+        System.out.println("=============================");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getObject(1)+"    "+resultSet.getObject(2)+"    "+resultSet.getObject(3));
+        }
+    }
+
+    public void viewByAgeLessThan(int age) throws SQLException {
+        statement = connection.prepareStatement("SELECT * FROM Person WHERE age < ?");
+        statement.setInt(1,age);
+        resultSet = statement.executeQuery();
+        System.out.println("idNumber |   name   |  age  |");
+        System.out.println("=============================");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getObject(1)+"    "+resultSet.getObject(2)+"    "+resultSet.getObject(3));
+        }
+    }
+
+    public void viewByAgeMoreThan(int age) throws SQLException {
+        statement = connection.prepareStatement("SELECT * FROM Person WHERE age > ?");
+        statement.setInt(1,age);
+        resultSet = statement.executeQuery();
+        System.out.println("idNumber |   name   |  age  |");
+        System.out.println("=============================");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getObject(1)+"    "+resultSet.getObject(2)+"    "+resultSet.getObject(3));
+        }
+    }
+
+    public void viewByEqualAge(int age) throws SQLException {
+        statement = connection.prepareStatement("SELECT * FROM Person WHERE age == ?");
+        statement.setInt(1,age);
+        resultSet = statement.executeQuery();
+        System.out.println("idNumber |   name   |  age  |");
+        System.out.println("=============================");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getObject(1)+"    "+resultSet.getObject(2)+"    "+resultSet.getObject(3));
+        }
+    }
+
+    public void viewByEqualName(String name) throws SQLException {
+        statement = connection.prepareStatement("SELECT * FROM Person WHERE name == ?");
+        statement.setString(1,name);
+        resultSet = statement.executeQuery();
+        System.out.println("idNumber |   name   |  age  |");
+        System.out.println("=============================");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getObject(1)+"    "+resultSet.getObject(2)+"    "+resultSet.getObject(3));
+        }
+    }
+
+    public void viewPeopleOrderByAge(byte order) throws SQLException {
+        String ord = "ASC";
+        if(order == 1) ord = "DESC";
+        statement = connection.prepareStatement("SELECT * FROM Person ORDER BY ?");
+        statement.setString(1,ord);
+        resultSet = statement.executeQuery();
+        System.out.println("idNumber |   name   |  age  |");
+        System.out.println("=============================");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getObject(1)+"    "+resultSet.getObject(2)+"    "+resultSet.getObject(3));
+        }
+    }
+
 }
